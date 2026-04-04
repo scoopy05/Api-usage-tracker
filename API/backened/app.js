a13b54c35aecaaf9b44a2060d94d9d09cd003427
scoopy05 <leoimran199@gmail.com>
2026-04-04 13:16:54 +0530

    last

--- API/backened/app.js
+++ API/backened/app.js
@@ -7,6 +7,10 @@ const cookieParser=require("cookie-parser");
 
 
 const app=express();
+app.use((req, res, next) => {
+  console.log("Request received:", req.method, req.url);
+  next();
+});
 
 app.use(cors({
   origin: [
--- API/backened/server.js
+++ API/backened/server.js
@@ -2,6 +2,13 @@ const app=require("./app");
 require("dotenv").config();
 const connectDB=require("./confiq/db");
 connectDB();
+app.get("/", (req, res) => {
+    res.send("SERVER.JS ROOT WORKING");
+  });
+  
+  app.get("/test", (req, res) => {
+    res.send("SERVER.JS TEST WORKING");
+  });
 
 const PORT = process.env.PORT || 8000;
 
