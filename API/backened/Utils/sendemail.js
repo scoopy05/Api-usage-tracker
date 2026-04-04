8dba1040ee07937532a72e43ca4576edc96d824b
scoopy05 <leoimran199@gmail.com>
2026-04-04 17:28:38 +0530

    Secured API and finalized auth

--- API/backened/Utils/sendemail.js
+++ API/backened/Utils/sendemail.js
@@ -1,28 +1,41 @@
+require('dotenv').config();
+const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
+
+const CLIENT_ID = process.env.GMAIL_CLIENT_ID;
+const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET;
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 const sendEmail = async (to, subject, html) => {
   try {
+    const accessToken = await oAuth2Client.getAccessToken();
+
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
+        type: "OAuth2",
+        user: process.env.GMAIL_USER,
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken.token,
+      },
     });
 
-    console.log("Email function called");
+    const mailOptions = {
+      from: `Your App <${process.env.GMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    };
 
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: to,
-      subject: subject,
-      html: html
-    });
-
-    console.log("Email sent:", info.response);
-
+    const result = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", result.response);
+    return result;
   } catch (error) {
     console.log("EMAIL ERROR:", error);
   }
