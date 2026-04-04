30379369ec8856b32f9129a2edda56e7591914cf
scoopy05 <leoimran199@gmail.com>
2026-04-04 11:47:06 +0530

    cors issue again

--- API/backened/app.js
+++ API/backened/app.js
@@ -3,6 +3,9 @@ const cors=require("cors");
 const cookieParser=require("cookie-parser");
 
 
+
+
+
 const app=express();
 
 const corsOptions = {
@@ -26,12 +29,13 @@ const authRoutes=require("./Routes/authRoutes");
 const userRoutes=require("./Routes/userRoutes");
 const apiRoutes=require("./Routes/apiRoutes");
 const planRoutes = require("./Routes/planRoutes");
-
+import proxyRoutes from "./Routes/proxyroutes.js";;
 
 app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
 app.use("/api",apiRoutes);
 app.use("/api", planRoutes);
+app.use("/proxy", proxyRoutes);;
 
 
 module.exports=app;
