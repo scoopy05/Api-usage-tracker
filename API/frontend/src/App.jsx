7c32b99137483ee33d3c6dd4f3f16b2733375229
scoopy05 <leoimran199@gmail.com>
2026-04-04 10:34:49 +0530

    nothing

--- API/frontend/src/App.jsx
+++ API/frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Auth from './pages/auth'
 import Dashboardlayout from './pages/dashboard/dashboardlayout';
 import Overview from './pages/dashboard/overview';
 import Usage from './pages/dashboard/usage';
-import Api from './pages/dashboard/API';
+import Api from './pages/dashboard/api';
 import Documentation from "./pages/Documentation";
 
 import ApiKeys from './pages/dashboard/ApiKeys';
@@ -25,7 +25,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboardlayout />}>
                <Route index element={<Overview />} />
                <Route path="usage" element={<Usage />} />
-               <Route path="api" element={<Api />} />
+               <Route path="api" element={<Api/>} />
               
                <Route path="keys" element={<ApiKeys />} />
                
