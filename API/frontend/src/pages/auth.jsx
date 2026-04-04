e1c50acb14231a12dd7a87a4da9d5045cf2c196c
scoopy05 <leoimran199@gmail.com>
2026-04-04 10:54:39 +0530

    path problem fixed

--- API/frontend/src/pages/auth.jsx
+++ API/frontend/src/pages/auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
-import './Auth.css';
+import './auth.css';
 import logo from '../assets/logo.png'
 import sideimg from '../assets/sideimg.png'
 import API from "../api/api";
