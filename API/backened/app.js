cb654aa86388ba9517b030cdf1d4d2a283525ab4
scoopy05 <leoimran199@gmail.com>
2026-04-04 13:41:59 +0530

    strees3

--- API/backened/app.js
+++ API/backened/app.js
@@ -7,47 +7,31 @@ console.log("APP.JS LOADED");
 
 
 
-const app=express();
-app.use((req, res, next) => {
-  console.log("Request received:", req.method, req.url);
-  next();
-});
 
-app.use(cors({
-  origin: [
-    "https://api-usage-tracker-ten.vercel.app",
-    "https://api-usage-tracker-git-main-scoopy05s-projects.vercel.app",
-    "http://localhost:5173",
-    "http://127.0.0.1:5500"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "x-api-key"]
-}));
+module.exports = (app) => {
 
+  app.use(cors());
+  app.use(express.json());
+  app.use(cookieParser());
 
+  app.get("/", (req, res) => {
+    res.send("API management server");
+  });
 
+  app.get("/test", (req, res) => {
+    res.send("Test route working");
+  });
 
-app.use(express.json());
-app.use(cookieParser());
+  const authRoutes = require("./Routes/authRoutes");
+  const userRoutes = require("./Routes/userRoutes");
+  const apiRoutes = require("./Routes/apiRoutes");
+  const planRoutes = require("./Routes/planRoutes");
+  const proxyRoutes = require("./Routes/proxyroutes");
 
-app.get("/",(req,res)=>{
-res.send(" API managemnt server")});
-app.get("/test", (req, res) => {
-  res.send("Test route working");
-});
+  app.use("/api/auth", authRoutes);
+  app.use("/api/user", userRoutes);
+  app.use("/api", apiRoutes);
+  app.use("/api", planRoutes);
+  app.use("/proxy", proxyRoutes);
 
-// const authRoutes=require("./Routes/authRoutes");
-// const userRoutes=require("./Routes/userRoutes");
-// const apiRoutes=require("./Routes/apiRoutes");
-// const planRoutes = require("./Routes/planRoutes");
-
-
-// app.use("/api/auth",authRoutes);
-// app.use("/api/user",userRoutes);
-// app.use("/api",apiRoutes);
-// app.use("/api", planRoutes);
-
-console.log("ROUTES LOADED");
-
-
-module.exports=app;
+};
--- API/backened/server.js
+++ API/backened/server.js
@@ -1,7 +1,14 @@
-const app=require("./app");
+const express = require("express");
+const app = express();
+
+
+
+
+
 require("dotenv").config();
 const connectDB=require("./confiq/db");
 connectDB();
+require("./app")(app);
 
 
 const PORT = process.env.PORT || 8000;
