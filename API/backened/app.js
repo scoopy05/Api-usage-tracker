c2158d0070b5aa471f3252a0b704ef07129d4e8b
scoopy05 <leoimran199@gmail.com>
2026-04-04 11:21:42 +0530

    cors problem solved

--- API/backened/app.js
+++ API/backened/app.js
@@ -5,7 +5,11 @@ const cookieParser=require("cookie-parser");
 
 const app=express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*", 
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type", "x-api-key", "Authorization"]
+  }));
 app.use(express.json());
 app.use(cookieParser());
 
